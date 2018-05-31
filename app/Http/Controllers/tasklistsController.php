59c1b49f2657cb43910130fce7815b58c4cafeb4
tsugumi.noshi <ec2-user@ip-172-31-12-246.us-east-2.compute.internal>
2018-05-31 23:50:28 +0000

    Done

--- app/Http/Controllers/tasklistsController.php
+++ app/Http/Controllers/tasklistsController.php
@@ -47,7 +47,7 @@ class tasklistsController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required|max:191',
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
         
@@ -99,7 +99,7 @@ class tasklistsController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'status' => 'required|max:191',
+            'status' => 'required|max:10',
             'content' => 'required|max:191',
         ]);
         
