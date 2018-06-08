aa3434ce54455a91915f5251c2913592f9ec6b8b
tsugumi.noshi <ec2-user@ip-172-31-12-246.us-east-2.compute.internal>
2018-06-08 01:08:08 +0000

    user registration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,23 +4,27 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Tasklist</title>
-         <!-- Bootstrap -->
+
+        <title>Tasklists</title>
+
+        <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     </head>
+    <body> 
+        @include('commons.navbar')
 
-    <body>
-        <div class="row">
-            <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-lg-offset-3 col-lg-6"></div>
+        <div class="container">
+            @include('commons.error_messages')
+            
+            @yield('content')
+            
+            <div class="row">
+                
+            </div>
+            
+            
         </div>
-    
-    @include('commons.navbar')
-    <div class="container">
-       @include('commons.error_tasklists')
-       @yield('content')
-      </div>
-       
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', 'tasklistsController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::resource('tasklists', 'tasklistsController');
+?>
